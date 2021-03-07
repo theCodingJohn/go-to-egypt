cd2d0f93214b4365d5567db9223556ce22d95a94
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-07 18:10:35 +0800

    finish adding content on hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,6 +12,39 @@ const Hero = () => {
               <br className="line-break"></br>Pyramids of Giza and ancient
               Egpyt's
             </h1>
+            <div className="scroll-down-wrapper">
+              <div className="arrow-wrapper">
+                <span className="arrow"></span>
+                <span className="arrow"></span>
+              </div>
+              <span className="scroll-down">SCROLL DOWN</span>
+            </div>
+            <div className="social-links-wrapper absolute">
+              <a
+                target="_blank"
+                href="https://www.facebook.com/"
+                rel="noreferrer noopener"
+              >
+                Fb
+              </a>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/"
+                rel="noreferrer noopener"
+              >
+                In
+              </a>
+              <a
+                target="_blank"
+                href="https://twitter.com/"
+                rel="noreferrer noopener"
+              >
+                Tw
+              </a>
+            </div>
+            <a href="mailto:hi@gotoegypt.com" className="email absolute">
+              hi@gotoegypt.com
+            </a>
           </div>
         </div>
       </div>
