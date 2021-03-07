00915a711d50bc3aa88c4b2835601600fc0ab279
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-07 18:53:18 +0800

    add navbar links

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,25 @@ const Header = () => {
               <span className="brown">EGYPT</span>
             </h3>
           </div>
-          <div className="col col2"></div>
+          <div className="col col2">
+            <ul className="link-wrapper">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">Explore</a>
+              </li>
+              <li>
+                <a href="/">Articles</a>
+              </li>
+              <li>
+                <a href="/">Galleries</a>
+              </li>
+              <li>
+                <a href="/">Contact</a>
+              </li>
+            </ul>
+          </div>
           <div className="col col3">
             <button className="button">
               <div className="circle circle-left circle-top"></div>
